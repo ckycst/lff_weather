c9d7f52597a4979c70a242d0e7f20d45cab3c47b
pinger <ckycst@gmail.com>
2018-08-23 08:41:30 -0400

    edit lff_weatherthread.py

--- lff_weatherthread.py
+++ lff_weatherthread.py
@@ -19,9 +19,6 @@ class WEATHER(object):
     }, timeout=10)
         self.result = self.result.json()
 
-    def _insertdata(self):
-        self.today = datetime.date.today()
-
         city  = self.result['results'][0]["location"]['id']
         high  = self.result['results'][0]["daily"][0]['high']
         low   = self.result['results'][0]["daily"][0]['low']
@@ -38,14 +35,11 @@ class WEATHER(object):
 def _generateWeatherDataO():
     city = CITYS()
     citylist1 = city._getcityenname1()
-    for c in citylist1:
-        print c
 
     db = MySQLdb.connect("localhost", "root", "!Cky2306mysql", "weather", port=3306)
     cursor = db.cursor()
     for c in citylist1:
         weather = WEATHER(c)
-        weather._insertdata()
 
     cursor.close()
     db.close()
@@ -53,14 +47,11 @@ def _generateWeatherDataO():
 def _generateWeatherDataT():
     city = CITYS()
     citylist2 = city._getcityenname2()
-    for c in citylist2:
-        print c
 
     db = MySQLdb.connect("localhost", "root", "!Cky2306mysql", "weather", port=3306)
     cursor = db.cursor()
-    for c in citylist1:
+    for c in citylist2:
         weather = WEATHER(c)
-        weather._insertdata()
 
     cursor.close()
     db.close()
